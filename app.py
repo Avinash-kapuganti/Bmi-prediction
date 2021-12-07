3ae6017bd1724a34a2040ce494164842c15f5f0b
avinash-2000 <71930072+avinash-2000@users.noreply.github.com>
2021-12-07 20:25:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,10 @@ def predict():
     int_features=[int(x) for x in request.form.values()]
     final=[np.array(int_features)]
     prediction=model.predict(final)
-   
-    
     l=["are Extremely weak","are Weak","have Normal weight","are Overweight","are Obese","have Extreme obesity"] 
     for i in prediction:
-        ans= l[i]
-    return render_template('BMI_predict.html',pred='Your {}'.format(ans))
+        output= l[i]
+    return render_template('BMI_predict.html',pred='Your {}'.format(output))
    
 
 
